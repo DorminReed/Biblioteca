d1c0c2bd2c665dbbfe7ee545ee3a9a22996d7a96
Pedro Magerl <58238094+DorminReed@users.noreply.github.com>
2024-05-03 17:34:19 -0300

    Add files via upload

---
+++ AnhangueraAtividade.sql
@@ -0,0 +1,35 @@
+Create DATABASE anangueraAtividade;
+USE anangueraAtividade;
+
+Create TABLE aluno (
+Ra INT PRIMARY KEY AUTO_INCREMENT,
+Nome VARCHAR(50) NOT NULL,
+Email VARCHAR(50) NOT NULL,
+Telefone VARCHAR(50) NOT NULL
+);
+
+Create TABLE livro (
+Isbn INT PRIMARY KEY AUTO_INCREMENT,
+Nome VARCHAR(50) NOT NULL,
+Autor VARCHAR(50) NOT NULL,
+Páginas INT,
+FOREIGN KEY (Isbn) REFERENCES aluno(Ra)
+);
+
+Create TABLE colaborador(
+Cpf INT PRIMARY KEY AUTO_INCREMENT,
+Nome VARCHAR(50) NOT NULL,
+Email VARCHAR(50) NOT NULL,
+Cargo VARCHAR(50) NOT NULL
+);
+
+Create TABLE empréstimo(
+Id INT PRIMARY KEY AUTO_INCREMENT,
+DataEmprestimo DATE,
+DataDevolucao DATE,
+LivrosIsbn INT,
+ColaboradorCpf INT,
+FOREIGN KEY (LivrosIsbn) REFERENCES livro(Isbn),
+FOREIGN KEY (ColaboradorCpf) REFERENCES colaborador(Cpf)
+);
+
